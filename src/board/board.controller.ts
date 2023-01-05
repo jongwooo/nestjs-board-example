fd4f9ad749281ad9749b2a54f56357c63e69752e
jongwooo <jongwooo.han@gmail.com>
2023-01-06 00:54:10 +0900

    style: Update files to format code

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -3,5 +3,5 @@ import { BoardService } from './board.service';
 
 @Controller('board')
 export class BoardController {
-	constructor(private boardService: BoardService) {}
+  constructor(private boardService: BoardService) {}
 }
