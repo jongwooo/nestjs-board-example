45f315f846c5a45360d889fbd6457db9796231a5
jongwooo <jongwooo.han@gmail.com>
2023-01-25 15:25:32 +0900

    feat: Add create-board dto

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('board')
 export class BoardController {
@@ -12,10 +13,7 @@ export class BoardController {
   }
 
   @Post('/')
-  createBoard(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Board {
-    return this.boardService.createBoard(title, description);
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto);
   }
 }
