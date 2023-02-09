fb3ac997d2e629a1a617f446ba5d37ea9f502999
jongwooo <jongwooo.han@gmail.com>
2023-02-10 00:38:46 +0900

    feat: Create board repository

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
+import { BoardRepository } from './board.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardRepository])],
   controllers: [BoardController],
   providers: [BoardService],
 })
