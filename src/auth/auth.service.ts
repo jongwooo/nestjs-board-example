3f5b55808de64ca54191f0e2307b2f015b56538d
jongwooo <jongwooo.han@gmail.com>
2023-02-14 23:23:09 +0900

    refactor: Remove redundant async await syntax

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
     private userRepository: UserRepository,
   ) {}
 
-  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
