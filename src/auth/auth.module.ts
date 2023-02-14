f1345e621fc74b0428cbff1168762b5c69acb4a5
jongwooo <jongwooo.han@gmail.com>
2023-02-15 00:07:13 +0900

    feat: Register PassportModule to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'Secret1234',
       signOptions: {
