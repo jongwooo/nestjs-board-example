854b8bd1ba016538fd0c328a4a3944248ff77167
jongwooo <jongwooo.han@gmail.com>
2023-02-14 14:32:11 +0900

    feat: Implement signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      }
+
+      throw new InternalServerErrorException();
+    }
   }
 }
